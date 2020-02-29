e202456494e33d17d03c0e257c5a766f8a3d3b08
jianglin521 <17610835880@163.com>
2020-02-29 15:44:14 +0800

    文件存储、gui

--- day03/07文件练习.py
+++ day03/07文件练习.py
@@ -1,35 +1,44 @@
-# encoding="utf-8" 设置打开文件编码
-f = open('record.txt', encoding="utf-8")
+# 拆分文件
+def split_file(file_name):
+  # encoding="utf-8" 设置打开文件编码
+  f = open(file_name, encoding="utf-8")
 
-boy = []
-girl = []
-count = 1
-for each_line in f:
-  if each_line[:6] != '======':
-    # 我们这里进行字符串分割
-    # print(each_line, '1111')
-    (role, line_spoken) = each_line.split(':', 1)
-    if role == '小甲鱼':
-      boy.append(line_spoken)
-    if role == '小客服':
-      girl.append(line_spoken)
-  else:
-    # 文件分别保存操作
-    file_name_boy = 'boy_' + str(count) + '.txt'
-    file_name_girl = 'gile_' + str(count) + '.txt'
+  boy = []
+  girl = []
+  count = 1
+  for each_line in f:
+    if each_line[:6] != '======':
+      # 我们这里进行字符串分割
+      # print(each_line, '1111')
+      (role, line_spoken) = each_line.split(':', 1)
+      if role == '小甲鱼':
+        boy.append(line_spoken)
+      if role == '小客服':
+        girl.append(line_spoken)
+    else:
+      # 保存文件
+      save_file(boy, girl, count)
 
-    boy_file = open(file_name_boy, 'w', encoding='utf-8')
-    girl_file = open(file_name_girl, 'w', encoding='utf-8')
+      boy = []
+      girl = []
+      count += 1
+  # 关闭文件
+  f.close()
 
-    boy_file.writelines(boy)
-    girl_file.writelines(girl)
+# 保存文件
+def save_file(boy, girl, count):
+  # 文件分别保存操作
+  file_name_boy = 'boy_' + str(count) + '.txt'
+  file_name_girl = 'gile_' + str(count) + '.txt'
 
-    boy_file.close()
-    girl_file.close()
+  boy_file = open(file_name_boy, 'w', encoding='utf-8')
+  girl_file = open(file_name_girl, 'w', encoding='utf-8')
 
-    boy = []
-    girl = []
-    count += 1
+  boy_file.writelines(boy)
+  girl_file.writelines(girl)
 
-# 关闭文件
-f.close()
+  boy_file.close()
+  girl_file.close()
+
+# 调用
+split_file('record.txt')
