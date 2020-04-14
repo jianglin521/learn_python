359f7e48b21c19663b5d133456d1076e08333e11
jianglin521 <17610835880@163.com>
2020-04-14 09:10:40 +0800

    修改

--- day09/01excel转json.py
+++ day09/01excel转json.py
@@ -4,10 +4,10 @@ import requests
 
 def openWorkbook():
     # 读取excel表的数据
-    workbook = xlrd.open_workbook('./专题数据45-50.xlsx')
+    workbook = xlrd.open_workbook('./专题数据51-76.xlsx')
     # 选取需要读取数据的那一页
-    #sheet = workbook.sheet_by_index(0)
-    sheet = workbook.sheet_by_name('00')
+    # sheet = workbook.sheet_by_index(0)
+    sheet = workbook.sheet_by_name('视频76')
     # 获得行数
     rows = sheet.nrows
     # 列数列数
